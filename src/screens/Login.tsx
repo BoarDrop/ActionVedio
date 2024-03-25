35628e3968117e67b31e00801d468931339faef1
Santa33333 <2735538048@qq.com>
2024-03-25 15:45:58 +0800

    修改Home、Login界面响应式布局样式，改为百分比

--- src/screens/Login.tsx
+++ src/screens/Login.tsx
@@ -1,8 +1,12 @@
-// 视频介绍界面
+// 进入app的介绍欢迎界面
 import React from 'react';
 import {View, StyleSheet, Text, Image, TouchableOpacity} from 'react-native';
 import {NavigationProp} from '@react-navigation/native';
 import Google from '../components/Google/Google';
+import {
+  widthPercentageToDP as wp,
+  heightPercentageToDP as hp,
+} from 'react-native-responsive-screen';
 
 interface LoginProps {
   navigation: NavigationProp<any>;
@@ -95,30 +99,39 @@ const styles = StyleSheet.create({
     backgroundColor: 'black',
   },
   google_btn: {
-    width: 320,
-    height: 60,
-    //backgroundColor: 'pink',
-    marginTop: -25,
+    // width: 320,
+    // height: 60,
+    // marginTop: -25,
+    width: wp('90%'), // 假设屏幕宽度为375pt，则此处约为320pt
+    height: hp('8%'), // 基于高度的百分比
+    marginTop: hp('-3%'),
   },
   word: {
-    width: 320,
-    height: 580,
-    marginTop: 35,
-    marginBottom: 40,
+    //width: 320,
+    //height: 580,
+    //marginTop: 35,
+    //marginBottom: 40,
+    width: wp('90%'),
+    height: hp('76.5%'), // 根据屏幕高度的百分比调整
+    marginTop: hp('4.6%'),
+    marginBottom: hp('5.3%'),
     justifyContent: 'space-between',
   },
   title: {
     width: '100%',
-    height: 60,
+    //height: 60,
+    height: hp('8%'),
   },
   title_inner: {
     color: 'white',
-    fontSize: 24,
+    //fontSize: 24,
+    fontSize: wp('6.7%'), // 基于屏幕宽度的百分比调整字体大小
     fontWeight: '700',
   },
   img: {
     width: '100%',
-    height: 190,
+    //height: 190,
+    height: hp('25%'),
   },
   img_inner: {
     width: '100%',
@@ -128,21 +141,25 @@ const styles = StyleSheet.create({
   },
   dear: {
     width: '100%',
-    height: 26,
+    //height: 26,
+    height: hp('3.4%'),
   },
   dear_inner: {
     color: 'white',
-    fontSize: 20,
+    //fontSize: 20,
+    fontSize: wp('5.6%'),
     fontWeight: '700',
   },
   content: {
     width: '100%',
-    height: 250,
+    //height: 250,
+    height: hp('33%'),
     justifyContent: 'space-between',
   },
   content_inner: {
     color: 'white',
-    fontSize: 12,
+    //fontSize: 12,
+    fontSize: wp('3.3%'),
     fontWeight: '700',
     lineHeight: 17,
   },
@@ -152,63 +169,75 @@ const styles = StyleSheet.create({
   },
   content_number: {
     color: 'white',
-    fontSize: 12,
+    //fontSize: 12,
+    fontSize: wp('3.3%'),
     fontWeight: '700',
     lineHeight: 17,
     marginRight: 5,
   },
   content_word: {
     color: 'white',
-    fontSize: 12,
+    //fontSize: 12,
+    fontSize: wp('3.3%'),
     fontWeight: '700',
     lineHeight: 17,
   },
   bottom: {
-    width: 320,
-    height: 60,
-    //backgroundColor: 'pink',
+    // width: 320,
+    // height: 60,
+    width: wp('90%'),
+    height: hp('8%'),
     justifyContent: 'space-between',
   },
   front: {
-    width: 320,
-    height: 25,
-    //backgroundColor: '#9747FF',
+    // width: 320,
+    // height: 25,
+    width: wp('90%'),
+    height: hp('3.3%'),
     justifyContent: 'center',
     flexDirection: 'row',
     alignItems: 'center',
-    marginTop: 5,
+    //marginTop: 5,
+    marginTop: hp('0.66%'),
   },
   line: {
-    width: 100,
+    //width: 100,
+    width: wp('28%'),
     height: 1, // 线条的高度
     backgroundColor: 'white', // 线条的颜色
   },
   or: {
     color: '#FFFFFF',
-    fontSize: 16,
+    //fontSize: 16,
+    fontSize: wp('4.4%'),
     fontWeight: '400',
     marginLeft: 20,
     marginRight: 20,
   },
   way: {
-    width: 320,
-    height: 35,
-    //backgroundColor: '#16D46B',
+    //width: 320,
+    //height: 35,
+    width: wp('90%'),
+    height: hp('4.6%'),
     justifyContent: 'center',
     alignItems: 'center',
     flexDirection: 'row',
-    gap: 10,
-    marginTop: -5,
+    //gap: 10,
+    gap: wp('3.2%'),
+    //marginTop: -5,
+    marginTop: hp('-0.66%'),
   },
   way_text: {
     color: '#FFFFFF',
-    fontSize: 15,
+    //fontSize: 15,
+    fontSize: wp('4.2%'),
     fontWeight: '400',
     //backgroundColor: 'pink',
   },
   sign_text: {
     color: '#FFFFFF',
-    fontSize: 15,
+    //fontSize: 15,
+    fontSize: wp('4.2%'),
     fontWeight: '400',
     textDecorationLine: 'underline',
   },
